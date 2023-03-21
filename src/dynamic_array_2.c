799458a9f30f9a733daaf5475805ec3108cf37a4
Daan <daandenhoed@gmail.com>
2023-03-21 18:26:36 +0100

    dynamic_array_2

--- src/dynamic_array_2.c
+++ src/dynamic_array_2.c
@@ -2,30 +2,33 @@
 #include <stdlib.h>
 
 int main() {
-  int* ptr;
+  int* ptr = NULL;
   int size;
 
   printf("Enter size of elements:");
   scanf("%d", &size);
 
-  ptr = (int*)malloc(size * sizeof(int));
+  ptr = malloc(size * sizeof(int));
 
   if (ptr == NULL) {
     printf("Memory not allocated.\n");
+    return 1;
   }
-  else {
-    printf("Memory successfully allocated.\n");
 
-    for (int i = 0; i < size; i++) {
-      ptr[i] = i + 1;
-    }
+  printf("Memory successfully allocated.\n");
 
-    printf("Array : [");
-    for(int j = 0; j < size; j++) {
-      printf(" %d ", ptr[j]);
-    }
-    printf(" ]\n");
+  for (int i = 0; i < size; i++) {
+    ptr[i] = i + 1;
   }
+
+  printf("Array : [");
+  for(int j = 0; j < size; j++) {
+    printf(" %d ", ptr[j]);
+  }
+  printf(" ]\n");
+
+  free(ptr)
+  ptr = NULL;
         
   return 0;
 }
