3f85bd8455aa97f34f25a6fe0b46fc85a7dee705
Daan <daandenhoed@gmail.com>
2023-03-23 21:41:26 +0100

    array init

--- src/array/array.c
+++ src/array/array.c
@@ -1,1 +1,17 @@
 
+void array_new(int size){
+  int *arr = (int*)malloc(size  * sizeof(int));
+  if (arr == NULL) {
+    printf("Error: unable to allocate memory!\n");
+    exit(1);
+  for (int i = 0; i < size; i++) {
+    arr[i] = 0;
+  }
+  free(arr);
+}
+
+int main() {
+  int size = 10;
+  array_new(size);
+  return 0;
+}
